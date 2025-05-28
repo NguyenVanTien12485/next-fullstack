03843aa7a7c208c6101d7434f5be9b8a33f83ee7
tien nguyen <tiennguyen@okxe.vn>
2025-05-29 02:56:13 +0700

    Implement UI landing page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import HeroSection from './HeroSection'
+import FeaturesSection from './FeaturesSection'
+import DiscoverSection from './DiscoverSection'
+import CallToActionSection from './CallToActionSection'
+import FooterSection from './FooterSection'
 const Landing = () => {
   return (
     <div>
-      Landing
+      <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
     </div>
   )
 }
